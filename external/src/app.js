9e84e8e65baeb84c348175585724128d9991d49f
aryanksh <aryanksh@gmail.com>
2023-08-15 00:18:37 -0400

    Prettier VSCode extension formatted code. no change in functionality

--- external/src/app.js
+++ external/src/app.js
@@ -1,26 +1,25 @@
 import React from "react";
-import NavBar from "./components/NavBar"; 
-import Footer from "./components/Footer"; 
-import SignUp from "./components/SignUp"; 
-import Container from "./components/Container"; 
-import {Routes, Route } from 'react-router-dom'; 
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import SignUp from "./components/SignUp";
+import Container from "./components/Container";
+import { Routes, Route } from "react-router-dom";
 
-  function App() {
-    return (
+function App() {
+  return (
     <div>
       <div className="intro">
         <NavBar />
         <Routes>
-            <Route path="/" element={<Container />} />
-            <Route path='/signup' element = {<SignUp />}/> 
-        </Routes> 
+          <Route path="/" element={<Container />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
       </div>
-      <div className = "footer">
+      <div className="footer">
         <Footer />
       </div>
-
     </div>
-    );
-  };
+  );
+}
 
-  export default App;
+export default App;
