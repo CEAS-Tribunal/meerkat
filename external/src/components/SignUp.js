b4546c8ea7d541c09566273545eebfb1564db009
aryanksh <aryanksh@gmail.com>
2023-08-15 00:49:43 -0400

    implemented standard contact info component

--- external/src/components/SignUp.js
+++ external/src/components/SignUp.js
@@ -1,17 +1,34 @@
-import React from "react"; 
+import React from "react";
+
+function ContactInfo() {
+  return (
+    <div className="contact-info">
+      <form>
+        <h2>Contact Information</h2>
+        <input type="text" id="name" name="name" placeholder="Name" autocomplete="off" />
+        <input type="text" id="mId" name="mId" placeholder="M# (Include the M)" autocomplete="off" />
+        <input type="text" id="email" name="email" placeholder="Email" autocomplete="off" />
+        <input type="text" id="phoneNum" name="phoneNum" placeholder="Phone Number" autocomplete="off" />
+      </form>
+    </div>
+  );
+}
 
 function SignUp() {
-    return (
-      <div>
-        <div className="title">
-          <h1> Sign Up Form</h1>
-        </div>
-        <div className="page-info">
-          <p>
-            Make the Sign Up Form here 
-          </p>
-        </div>
-      </div> 
-    );
-  };
-  export default SignUp;
+  return (
+    <div className="container">
+      <div className="title">
+        <h1> [Event Name] Sign Up Form</h1>
+      </div>
+      <div className="page-info">
+        <p>
+          This is placeholder text for the description of the event. We will probably have a variable (passed in as a prop) that contains the entire description
+          of the event. This way, we'll ideally have a different description for each event. The description is decided by the user filling out the internal
+          site.
+        </p>
+        <ContactInfo />
+      </div>
+    </div>
+  );
+}
+export default SignUp;
