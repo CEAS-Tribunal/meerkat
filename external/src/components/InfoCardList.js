0f7a43686896e02debcf23a65a6f2c6c7ce9864c
Athulya Ganesh <athulya.ganesh2001@gmail.com>
2023-10-18 21:56:02 -0400

    button stylesss

--- external/src/components/InfoCardList.js
+++ external/src/components/InfoCardList.js
@@ -1,6 +1,7 @@
  import React from "react"; 
  import event_img from "../assets/images/tribunal_logo_large.png"; // use in Infocard component file. add a comment to use this image IF an image is not provided by the
- import {Link} from 'react-router-dom'
+ //import {Link} from 'react-router-dom'
+ 
 
  // We will need to create dynamic InfoCards. Every piece of information will be stored in a variable
   function InfoCard() {
@@ -14,13 +15,19 @@
     if (description.length > 100) {
       description = description.substring(0, 100) + "...";
     }
+
   
+    function SignUpButton()
+    {
+      window.location.href = "https://www.google.com"
+    }
     return (
       <article className="infocard">
+        <h1 className = "title">{eventName}</h1>
         <img src={event_img} alt="test img" />
         <br />
         <div>
-      <Link to='/signup'> {eventName} --Sign Up Here! </Link>
+      <button className="signup-button infocard" onClick={SignUpButton}>Sign Up Here</button>
       </div>
         <h2>
           <b>Date/Time:</b> {date} from {time}
